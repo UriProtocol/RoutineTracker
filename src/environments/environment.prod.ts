57549c5f5df31292ff5ecc8746a7109622b2aec9
UrielGon03 <uriel@advantedigital.com>
2024-08-15 14:56:16 -0600

    adding environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDlmeBDFzuFX2oB39KpeLr7zPsbEo58hTo",
+    authDomain: "routinetracker-ad073.firebaseapp.com",
+    projectId: "routinetracker-ad073",
+    storageBucket: "routinetracker-ad073.appspot.com",
+    messagingSenderId: "390644028092",
+    appId: "1:390644028092:web:40284ebae27e79dd0836b9"
+  }
+};
