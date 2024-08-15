b5cda8ff5c0b964703ebda1fbac785473f96a08b
UrielGon03 <uriel@advantedigital.com>
2024-08-15 13:52:01 -0600

    name change

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'notes_app',
+  appName: 'routine_tracker',
   webDir: 'www'
 };
 
